c6e38b48075ffb29892489e03a35baa70f5d8994
Volker <skydiablo@gmx.net>
2025-10-17 10:06:46 +0200

    reformat code

--- src/FreeDSx/Snmp/SnmpClient.php
+++ src/FreeDSx/Snmp/SnmpClient.php
@@ -29,31 +29,32 @@ class SnmpClient
     /**
      * @var array
      */
-    protected $options = [
-        'transport' => 'udp',
-        'use_tls' => false,
-        'ssl_validate_cert' => true,
-        'ssl_allow_self_signed' => null,
-        'ssl_ca_cert' => null,
-        'ssl_peer_name' => null,
-        'port' => 161,
-        'host' => 'localhost',
-        'user' => null,
-        'community' => 'public',
-        'udp_retry' => 5,
-        'timeout_connect' => 5,
-        'timeout_read' => 10,
-        'version' => 2,
-        'security_model' => 'usm',
-        'engine_id' => null,
-        'context_name' => null,
-        'use_auth' => false,
-        'use_priv' => false,
-        'auth_mech' => null,
-        'priv_mech' => null,
-        'priv_pwd' => null,
-        'auth_pwd' => null,
-    ];
+    protected $options
+        = [
+            'transport'             => 'udp',
+            'use_tls'               => false,
+            'ssl_validate_cert'     => true,
+            'ssl_allow_self_signed' => null,
+            'ssl_ca_cert'           => null,
+            'ssl_peer_name'         => null,
+            'port'                  => 161,
+            'host'                  => 'localhost',
+            'user'                  => null,
+            'community'             => 'public',
+            'udp_retry'             => 5,
+            'timeout_connect'       => 5,
+            'timeout_read'          => 10,
+            'version'               => 2,
+            'security_model'        => 'usm',
+            'engine_id'             => null,
+            'context_name'          => null,
+            'use_auth'              => false,
+            'use_priv'              => false,
+            'auth_mech'             => null,
+            'priv_mech'             => null,
+            'priv_pwd'              => null,
+            'auth_pwd'              => null,
+        ];
 
     /**
      * @var ClientProtocolHandler|null
@@ -79,9 +80,10 @@ class SnmpClient
     /**
      * Send a bulk request and get the OidList back.
      *
-     * @param int $maxRepetitions
-     * @param int $nonRepeaters
+     * @param int        $maxRepetitions
+     * @param int        $nonRepeaters
      * @param string|Oid ...$oids
+     *
      * @return PromiseInterface<OidList>
      * @throws ConnectionException
      * @throws SnmpRequestException
@@ -89,11 +91,13 @@ class SnmpClient
     public function getBulk(
         int $maxRepetitions,
         int $nonRepeaters,
-            ...$oids
-    ): PromiseInterface
-    {
-        return $this->sendAndReceive(Requests::getBulk($maxRepetitions, $nonRepeaters, ...$oids))->then(function ($response) {
-            return $response->getResponse()
+        ...$oids
+    ): PromiseInterface {
+        return $this->sendAndReceive(
+            Requests::getBulk($maxRepetitions, $nonRepeaters, ...$oids),
+        )->then(function ($response) {
+            return $response
+                ->getResponse()
                 ->getOids();
         });
     }
@@ -102,39 +106,47 @@ class SnmpClient
      * Send a get next request to get the next variable(s) in the MIB tree back.
      *
      * @param string|Oid ...$oids
+     *
      * @return PromiseInterface<OidList>
      * @throws ConnectionException
      * @throws SnmpRequestException
      */
     public function getNext(...$oids): PromiseInterface
     {
-        return $this->sendAndReceive(Requests::getNext(...$oids))->then(function ($response) {
-            return $response->getResponse()
-                ->getOids();
-        });
+        return $this->sendAndReceive(Requests::getNext(...$oids))->then(
+            function ($response) {
+                return $response
+                    ->getResponse()
+                    ->getOids();
+            },
+        );
     }
 
     /**
      * Get any number of OID objects as an OidList.
      *
      * @param string|Oid ...$oids
+     *
      * @return PromiseInterface<OidList>
      * @throws ConnectionException
      * @throws SnmpRequestException
      */
     public function get(...$oids): PromiseInterface
     {
-        return $this->sendAndReceive(Requests::get(...$oids))->then(function ($response) {
-            return $response->getResponse()
-                ->getOids();
-        });
-
+        return $this->sendAndReceive(Requests::get(...$oids))->then(
+            function ($response) {
+                return $response
+                    ->getResponse()
+                    ->getOids();
+            },
+        );
     }
 
     /**
      * Get a single OID object. This contains the value object. If it doesn't exist, null is returned.
      *
      * @param string|Oid $oid
+     *
      * @return PromiseInterface<Oid|null>
      * @throws ConnectionException
      * @throws SnmpRequestException
@@ -150,6 +162,7 @@ class SnmpClient
      * Get the string value of an OID. If it doesn't exist, it will return null.
      *
      * @param string|Oid $oid
+     *
      * @return PromiseInterface<null|string>
      * @throws ConnectionException
      * @throws SnmpRequestException
@@ -165,6 +178,7 @@ class SnmpClient
      * Set one, or many, OID values.
      *
      * @param Oid ...$oids
+     *
      * @return PromiseInterface<MessageResponseInterface>
      * @throws ConnectionException
      * @throws SnmpRequestException
@@ -178,9 +192,10 @@ class SnmpClient
      * Sends an SNMP v2/3 style trap to a host.
      *
      * @param int|TimeTicksValue $sysUpTime
-     * @param string|Oid $trapOid
-     * @param Oid ...$oids
-     * @return SnmpClient
+     * @param string|Oid         $trapOid
+     * @param Oid                ...$oids
+     *
+     * @return PromiseInterface
      * @throws ConnectionException
      * @throws SnmpRequestException
      */
@@ -188,15 +203,14 @@ class SnmpClient
         $sysUpTime,
         $trapOid,
         ...$oids
-    ): self
-    {
-        $this->send(Requests::trap(
-            $sysUpTime,
-            $trapOid,
-            ...$oids
-        ));
-
-        return $this;
+    ): PromiseInterface {
+        return $this->send(
+            Requests::trap(
+                $sysUpTime,
+                $trapOid,
+                ...$oids,
+            ),
+        );
     }
 
     /**
@@ -204,10 +218,11 @@ class SnmpClient
      *
      * @param string $enterprise
      * @param string $address
-     * @param int $genericType
-     * @param int $specificType
-     * @param int $sysUpTime
-     * @param mixed ...$oids
+     * @param int    $genericType
+     * @param int    $specificType
+     * @param int    $sysUpTime
+     * @param mixed  ...$oids
+     *
      * @return $this
      * @throws ConnectionException
      * @throws SnmpRequestException
@@ -215,20 +230,21 @@ class SnmpClient
     public function sendTrapV1(
         string $enterprise,
         string $address,
-        int    $genericType,
-        int    $specificType,
-        int    $sysUpTime,
-               ...$oids
-    ): self
-    {
-        $this->send(Requests::trapV1(
-            $enterprise,
-            $address,
-            $genericType,
-            $specificType,
-            $sysUpTime,
-            ...$oids
-        ));
+        int $genericType,
+        int $specificType,
+        int $sysUpTime,
+        ...$oids
+    ): self {
+        $this->send(
+            Requests::trapV1(
+                $enterprise,
+                $address,
+                $genericType,
+                $specificType,
+                $sysUpTime,
+                ...$oids,
+            ),
+        );
 
         return $this;
     }
@@ -237,8 +253,9 @@ class SnmpClient
      * Sends an Inform request to a host. This is a v2/3 trap that requires a response from the host.
      *
      * @param int|TimeTicksValue $sysUpTime
-     * @param string|Oid $trapOid
-     * @param Oid ...$oids
+     * @param string|Oid         $trapOid
+     * @param Oid                ...$oids
+     *
      * @return PromiseInterface<MessageResponseInterface>
      * @throws ConnectionException
      * @throws SnmpRequestException
@@ -247,13 +264,14 @@ class SnmpClient
         $sysUpTime,
         $trapOid,
         ...$oids
-    ): PromiseInterface
-    {
-        return $this->sendAndReceive(Requests::inform(
-            $sysUpTime,
-            $trapOid,
-            ...$oids
-        ));
+    ): PromiseInterface {
+        return $this->sendAndReceive(
+            Requests::inform(
+                $sysUpTime,
+                $trapOid,
+                ...$oids,
+            ),
+        );
     }
 
     /**
@@ -261,17 +279,17 @@ class SnmpClient
      *
      * @param null|string $startAt
      * @param null|string $endAt
+     *
      * @return SnmpWalk
      */
     public function walk(
         string $startAt = null,
         string $endAt = null
-    ): SnmpWalk
-    {
+    ): SnmpWalk {
         return new SnmpWalk(
             $this,
             $startAt,
-            $endAt
+            $endAt,
         );
     }
 
@@ -279,20 +297,20 @@ class SnmpClient
      * Send a generic SNMP request and get the SNMP response back. Note that some requests do not generate a response.
      * In this case it will return null.
      *
-     * @param Pdu $request
+     * @param Pdu   $request
      * @param array $options
+     *
      * @return PromiseInterface<MessageResponseInterface>
      * @throws ConnectionException
      * @throws Exception\SnmpRequestException
      */
     public function send(
-        Pdu   $request,
+        Pdu $request,
         array $options = []
-    ): PromiseInterface
-    {
+    ): PromiseInterface {
         return $this->dispatcher()->handle(
             $request,
-            array_merge($this->options, $options)
+            array_merge($this->options, $options),
         );
     }
 
@@ -300,25 +318,28 @@ class SnmpClient
      * Same as the send method, but guarantees a response will be returned. An exception will be thrown if no response
      * is received.
      *
-     * @param Pdu $request The request to send.
+     * @param Pdu   $request The request to send.
      * @param array $options Any options for sending.
+     *
      * @return PromiseInterface<MessageResponseInterface>
      * @throws ConnectionException
      * @throws SnmpRequestException
      * @throws RuntimeException
      */
     public function sendAndReceive(
-        Pdu   $request,
+        Pdu $request,
         array $options = []
-    ): PromiseInterface
-    {
+    ): PromiseInterface {
         return $this->send(
             $request,
-            $options
+            $options,
         )->then(function ($response) {
             if ($response === null) {
-                throw new RuntimeException('Expected an SNMP response, but non was received.');
+                throw new RuntimeException(
+                    'Expected an SNMP response, but non was received.',
+                );
             }
+
             return $response;
         });
     }
@@ -347,6 +368,7 @@ class SnmpClient
      * Set the client options.
      *
      * @param array $options
+     *
      * @return $this
      */
     public function setOptions(array $options): self
@@ -362,7 +384,8 @@ class SnmpClient
     protected function dispatcher(): ClientProtocolHandler
     {
         if (!$this->handler) {
-            $this->handler = $this->options['_protocol_handler'] ?? new ClientProtocolHandler($this->options);
+            $this->handler = $this->options['_protocol_handler'] ??
+                new ClientProtocolHandler($this->options);
         }
 
         return $this->handler;
